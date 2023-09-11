89a43b2a3e2af9c0929802ef12a0204be44fc63e
wanderingbookworm <oforiestelle@gmail.com>
2023-09-11 12:44:59 -0700

    Submissions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - program starts and ends here
+ * return; always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
